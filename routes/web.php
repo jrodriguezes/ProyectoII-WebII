e88fd02be4c8144eaec8efdc53d9c157c34e044c
Miguel Cruz Vargas <miguelcruzvargas12@gmail.com>
2025-11-21 22:01:58 -0600

    mas arrglos de implementacion

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,14 @@ Route::get('/', function () {
     return view('booking');
 });
 
+Route::get('/booking', function () {
+    return view('booking');
+});
+
 Route::get('/login', function () {
     return view('login');
 });
 
-Route::get('//user-register', function () {
+Route::get('/user-register', function () {
     return view('user-register');
 });
